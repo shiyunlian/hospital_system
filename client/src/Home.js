e51a27606c3851307c5e37b96ea1990069e23fea
shiyunlian <shiyunlian00@gmail.com>
2021-07-15 23:03:50 -0700

    complete home page

---
+++ client/src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SignIn from "./components/SignIn";
+import Header from "./components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <SignIn />
+    </>
+  );
+};
+
+export default Home;
