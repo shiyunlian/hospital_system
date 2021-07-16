cd0c208ae5ac95ef1fb9302b36be7fd2541daf71
shiyunlian <shiyunlian00@gmail.com>
2021-07-15 23:08:06 -0700

    set up server listens on port 3000

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("");
+});
+
+app.get("/");
+
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
