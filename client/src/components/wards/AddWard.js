f7e04dd3ce14b4a220b6756e646cbe771f2c5bc6
THANH LE <thanhlevan0203@gmail.com>
2021-07-28 12:54:31 -0700

    Create Add and Update Ward Component

---
+++ client/src/components/wards/AddWard.js
@@ -0,0 +1,81 @@
+import Axios from 'axios';
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
+
+/****************************************************************************************
+ * Create a file for adding a ward and create stateful component AddWard
+ *****************************************************************************************/
+
+const AddWard = () => {
+    let history = useHistory();
+    const [ward, setWard] = useState({
+        wardID: '',
+        bedNumber: '',
+        patientID: ''
+    });
+    
+   const { wardID, bedNumber, patientID} = ward;
+
+   const onInputChange = e => {
+       setWard({ ...ward, [e.target.name]: e.target.value });
+   };
+
+   //post add ward record to db.json
+   const onSubmit = async e => {
+       e.preventDefault();
+       await Axios.post("http://localhost:8000/wards", ward);
+       history.push("/wards");
+   };
+
+
+    return(
+        <div className="container bg-light">
+            <div className="w-75 mx-auto shadow p-5">
+                <h2 className="text-center mb-4">Add A ward</h2>
+                    <form onSubmit={e => onSubmit(e)}>
+
+                    <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control form-control-lg"
+                                placeholder="Enter ward ID"
+                                name="wardID"
+                                value={wardID}
+                                onChange={ e => onInputChange(e)}
+                            />
+                        </div>
+                
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Bed Number"
+                                name="bedNumber"
+                                value={bedNumber}
+                                onChange={ e => onInputChange(e)}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Patient ID"
+                                name="patientID"
+                                value={patientID}
+                                onChange={ e => onInputChange(e)}
+                            />
+                        </div>
+
+                        
+                        <button className="btn btn-primary btn-block">Add Ward</button>
+            </form>
+            </div>
+        </div>
+    
+    
+        
+    )
+};
+
+export default AddWard;
