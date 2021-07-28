eeebbd42fadd07be8e8fefb3fce14050db654eba
THANH LE <thanhlevan0203@gmail.com>
2021-07-28 12:56:13 -0700

    Delete unnecessary comments

--- client/src/components/PatientRecord.js
+++ client/src/components/PatientRecord.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import useStyles from "./styles";
-//import PatientTable from "./PatientTable";
 import SignInHeader from "./SignInHeader";
-//import ListPatientComponent from "./ListPatientComponent";
 import ListPatient from "./ListPatient";
-import AddPatient from "./patients/AddPatient";
-
 
 
 export default function PatientRecord() {
@@ -20,8 +16,6 @@ export default function PatientRecord() {
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Grid item xs={14}>
-         {/*<PatientTable /> */} 
-         {/* <ListPatientComponent /> */}
             <ListPatient />
 
         </Grid>
