d1050bea2647635c43d260b4d59fa73a3e7923e3
THANH LE <thanhlevan0203@gmail.com>
2021-07-30 22:47:45 -0700

    add url

--- client/src/components/ListWard.js
+++ client/src/components/ListWard.js
@@ -1,56 +1,50 @@
-import  React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom'
-import {Link as NavLink} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Link as NavLink } from "react-router-dom";
 
-import {
-  Navbar,
-  Nav,
-  NavItem,
-  NavbarBrand,
-  Container
-} from 'reactstrap';
-
+import { Navbar, Nav, NavItem, NavbarBrand, Container } from "reactstrap";
 /***************************************************************
  Create a file for showing Ward list and delete records
  * ************************************************************/
-const Ward= () => {
-    const [wards, setWard] = useState([]);
 
-    useEffect(() => {
-        loadWards();
-    }, []);
+const url = "http://localhost:8090/wards";
 
-    //Get request using Axios http
-    const loadWards = async () => {
-        const result = await axios.get("http://localhost:8000/wards");
-        setWard(result.data.reverse());
-    };
+const Ward = () => {
+  const [wards, setWard] = useState([]);
 
-    //delete Ward records using axios http
-    const deleteWard = async id => {
-        await axios.delete('http://localhost:8000/wards/' + id);
-        loadWards();
-    };
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setWard(response.data);
+    });
+  }, []);
 
+  if (!wards) return null;
+  // useEffect(() => {
+  //   loadWards();
+  // }, []);
 
-    return  ( 
-        <div className="container" >
-            <div className="py-4">
-                <h1>Ward Information</h1>
-                <Navbar color="dark" dark>
-                    <Container>
-                    <NavbarBrand href="/wards">Ward List</NavbarBrand>
-                    <Nav>
-                        <Link className="btn btn-primary" to ="/wards/add">Add Ward</Link>
-                    </Nav>
-                    </Container>
+  // //Get request using Axios http
+  // const loadWards = async () => {
+  //   const result = await axios.get("http://localhost:8080/wards");
+  //   setWard(result.data.reverse());
+  // };
 
-                </Navbar>
-                <div className = "row">
-                 
-                 </div>
-                 <br></br>
+  // //delete Ward records using axios http
+  // const deleteWard = async id => {
+  //     await axios.delete('http://localhost:8000/wards/' + id);
+  //     loadWards();
+  // };
+
+  const deleteWard = (id) => {
+    axios.delete(`${url}/${id}`).then(() => {
+      alert("Ward record deleted!");
+    });
+  };
+
+  const test = () => {
+    console.log(wards);
+  };
 
                 <table class="table border shadow">
                     <thead class="thead-dark">
@@ -74,18 +68,61 @@ const Ward= () => {
                                         <Link class="btn btn-outline-primary mr-2" 
                                             to={'/wards/edit/${ward.id}'}>Edit</Link>
 
-                                        <Link class="btn btn-danger" 
-                                            onClick={() => deleteWard(ward.id)}>Delete</Link>
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>Ward Information</h1>
+        <Navbar color="dark" dark>
+          <Container>
+            <NavbarBrand href="/wards">Ward List</NavbarBrand>
+            <Nav>
+              <Link className="btn btn-primary" to="/wards/add">
+                Add Ward
+              </Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <div className="row"></div>
+        <br></br>
+        <table class="table border shadow">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Number</th>
+              <th scope="col">Ward ID</th>
+              <th scope="col">Bed Number</th>
+              <th scope="col">Patient ID</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {wards.map((ward, index) => (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{ward.WARDID}</td>
+                <td>{ward.BEDID}</td>
+                <td>{ward.PATIENTID}</td>
+                <td>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    to={"/wards/edit/${ward.id}"}
+                  >
+                    Edit
+                  </Link>
 
-                                    </td>
-                                </tr>                              
-                            ) )
-                        }
-                    </tbody>
-                    </table>            
-            </div>
-        </div> 
-    );
+                  <Link
+                    class="btn btn-danger"
+                    onClick={() => deleteWard(ward.WARDID)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Ward;
